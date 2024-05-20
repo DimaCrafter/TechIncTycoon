5ccf7c4ccbec8c663b91c0cb2b7d172d19ddfd91
Bin4h <snowz0230@gmail.com>
2024-05-20 20:59:05 +0400

    Сделал диалоговый текст и задания

--- Assets/Prefab/ResearchModal.cs
+++ Assets/Prefab/ResearchModal.cs
@@ -49,22 +49,116 @@ public class ResearchModal: Modal {
             }
         },
         new() {
-            title = "Передача данных",
-            description = "Лорем ипсум долор сит эммет...",
-            requiredResearchScore = 8,
+            title = "Проверить проводку",
+            description = "Модуль издает странные звуки, стоить проверить...",
+            requiredResearchScore = 14,
             requiredScienceScore = 7,
-            outResearchScore = 6,
-            outScienceScore = 5,
-            duration = 15
+            outResearchScore = 8,
+            outScienceScore = 1,
+            duration = 80,
+            events = new UnitTaskEvent[] {
+                new() { time = 2, research = 1 },
+                new() { time = 10, research = 2 },
+                new() { time = 16, research = 0, science = 4 },
+                new() { time = 48, research = 1 },
+                new() { time = 52, research = 2 },
+                new() { time = 68, research = 1 },
+                new() { time = 80, research = 1 },
+            }
         },
         new() {
-            title = "Моделирование вычислений",
-            description = "Лорем ипсум долор сит эммет...",
-            requiredResearchScore = 9,
-            requiredScienceScore = 10,
+            title = "Подготовить перфокарту",
+            description = "Первый шаг к программированию",
+            requiredResearchScore = 22,
+            requiredScienceScore = 8,
+            outResearchScore = 4,
+            outScienceScore = 13,
+            duration = 45,
+            events = new UnitTaskEvent[] {
+                new() { time = 2, research = 1, science = 4 },
+                new() { time = 10, research = 2, science = 1 },
+                new() { time = 16, research = 1, science = 2 },
+                new() { time = 45, research = 1, science = 7 }
+            }
+        },
+        new() {
+            title = "Откалибровать модуль",
+            description = "Настройка перед реальной работой",
+            requiredResearchScore = 26,
+            requiredScienceScore = 21,
+            outResearchScore = 10,
+            outScienceScore = 1,
+            duration = 145,
+            events = new UnitTaskEvent[] {
+                new() { time = 2, research = 1 },
+                new() { time = 10, research = 1 },
+                new() { time = 16, research = 1, science = 1 },
+                new() { time = 48, research = 1 },
+                new() { time = 52, research = 1 },
+                new() { time = 68, research = 1 },
+                new() { time = 80, research = 1 },
+                new() { time = 101, research = 3}
+            }
+        },
+        new() {
+            title = "Написать программу",
+            description = "Настал час",
+            requiredResearchScore = 36,
+            requiredScienceScore = 22,
+            outResearchScore = 20,
+            outScienceScore = 25,
+            duration = 145,
+            events = new UnitTaskEvent[] {
+                new() { time = 2, research = 2 },
+                new() { time = 10, research = 2 },
+                new() { time = 16, research = 2, science = 10 },
+                new() { time = 48, research = 1 },
+                new() { time = 52, research = 2 },
+                new() { time = 68, research = 2 },
+                new() { time = 80, research = 1 },
+                new() { time = 101, research = 2, science = 10 },
+                new() { time = 115, research = 2 },
+                new() { time = 130, research = 2 },
+                new() { time = 140, research = 2 }
+            }
+        },
+        new() {
+            title = "Исследовать модуль",
+            description = "Полный цикл разработки программы",
+            requiredResearchScore = 56,
+            requiredScienceScore = 47,
             outResearchScore = 11,
             outScienceScore = 12,
-            duration = 30
+            duration = 60,
+            events = new UnitTaskEvent[] {
+                new() { time = 2, research = 1, science = 3 },
+                new() { time = 10, research = 2, science = 1 },
+                new() { time = 16, research = 1, science = 2 },
+                new() { time = 48, research = 4, science = 2 },
+                new() { time = 52, research = 3, science = 4 }
+            }
+        },
+        new() {
+            title = "Технический осмотр модуля",
+            description = "Примените на практике все, чему научились",
+            requiredResearchScore = 67,
+            requiredScienceScore = 59,
+            outResearchScore = 24,
+            outScienceScore = 30,
+            duration = 145,
+            events = new UnitTaskEvent[] {
+                new() { time = 2, research = 1 },
+                new() { time = 10, research = 2 },
+                new() { time = 16, research = 1, science = 2 },
+                new() { time = 48, research = 4 },
+                new() { time = 52, research = 2 },
+                new() { time = 68, research = 1 },
+                new() { time = 80, research = 3 },
+                new() { time = 101, research = 2, science = 2 },
+                new() { time = 115, research = 1 },
+                new() { time = 130, research = 2 },
+                new() { time = 140, research = 1 }
+            }
         }
     };
 
